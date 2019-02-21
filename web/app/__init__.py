82ede2d78db34864f6f9982fe76210a716afd515
Alonso Muñoz <33006411+Devidences@users.noreply.github.com>
2019-02-22 00:30:03 +0100

    plantilla para web de ejemplo

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,6 +1,9 @@
 # crea el objeto app como una instancia de la class Flask
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+Bootstrap(app)
 
 from app import routes
--- web/app/routes.py
+++ web/app/routes.py
@@ -1,7 +1,11 @@
 from app import app
 from flask import render_template
+from flask_bootstrap import Bootstrap
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html')
+	
+if __name__ == '__main__':
+    app.run(debug=True)
