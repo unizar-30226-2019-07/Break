b18176c07f3dd8a3e9c8c142482485003ac2ad8e
Pablo <pabloorduna98@gmail.com>
2019-02-21 22:12:28 +0100

    Begin the login page

--- web/app/routes.py
+++ web/app/routes.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Log In')
