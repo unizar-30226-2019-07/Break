003e5ddc90bebce773264113a7b44eccc8f7bf90
end222 <pabloorduna98@gmail.com>
2019-02-26 10:33:09 +0100

    Solucionar problemas causados por el merge

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,7 +1,8 @@
 # crea el objeto app como una instancia de la class Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect
 from flask_bootstrap import Bootstrap
 from config import Config
+from app.forms import LoginForm
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +13,24 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if 'username' in session:
+        username = session['username']
+        print("Ya estabas logeado como " + username)
+    if form.is_submitted():
+        print("Usuario: " + form.username.data)
+        print("Contraseña: " + form.password.data)
+        session['username'] = form.username.data
+        return redirect('/index')
+    return render_template('login.html', title='Log In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/login')
 
 if __name__ == '__main__':
     app.run(debug=True)
