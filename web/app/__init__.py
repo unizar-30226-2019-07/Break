9071cf5de98921a7083636a4c40908328384ea92
Alonso Muñoz <33006411+Devidences@users.noreply.github.com>
2019-03-10 01:11:02 +0100

    Imagenes beta casi terminado

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, session, redirect
-from flask_bootstrap import Bootstrap
+# Para poder acceder a herramientas del sistema operativo
+import os
+
+from flask import Flask, render_template, session, redirect, request, send_from_directory
+# from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, current_user, login_user, logout_user
 from config import Config
 from app.forms import LoginForm
@@ -16,7 +19,10 @@ login = LoginManager(app)
 from app import models
 from app.models import User
 
-Bootstrap(app)
+# Constante con la dirección del servidor.
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Bootstrap(app)
 
 @login.user_loader
 def load_user(id):
@@ -75,13 +81,46 @@ def contact():
 def listing():
     return render_template('listings.html')
 
-@app.route('/single')
-def single():
-    return render_template('single.html')
-
 @app.route('/venderObjeto')
 def venderObjeto():
     return render_template('venderObjeto.html')
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    # Creamos la ruta donde vamos a guardar las imagenes
+    target = os.path.join(APP_ROOT, 'static/client_images/')
+    print(target)
+
+    # Si no existe la carpeta, la creamos.
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    #T enemos que hacer un bucle para guardar/enviar todas las imagenes que se quieren subir
+    # (El cliente puere queder subir varias)
+    for file in request.files.getlist("file"):
+        print(file) #Debug
+        # Cogemos el nombre del archivo como nombre que se va a guardar, por ahora.
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)  #Debug
+        file.save(destination)
+
+    # Redirige a la ruta deseada, se pueden pasar parametros
+    return redirect("/single")
+
+# Devuelve las imagenes de un directorio
+@app.route('/imagenes/<filename>')
+def send_image(filename):
+    return send_from_directory("static/client_images", filename)
+
+@app.route('/single')
+def get_gallery():
+    #Devuelve un vector de nombres de imgenes de la ruta especificada
+    image_names = os.listdir('./static/client_images')
+    print(image_names)  #Debug
+    return render_template("single.html", image_names=image_names)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
